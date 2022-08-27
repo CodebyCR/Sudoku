2f9e7a3c41f91fe621d128e902801be2b94906b5
CR <44606665+CodebyCR@users.noreply.github.com>
2022-08-27 22:01:17 +0200

    Add SFML GUI

--- Sudoku/Main.cpp
+++ Sudoku/Main.cpp
@@ -1,65 +1,72 @@
 #include <iostream>
-//#include "Cell.cpp"
 #include <map>
+#include <SFML/Graphics.hpp>
+#include "Matrix.hpp"
 
-std::map<int, int> rowValues;
 
 
 
-/**
- @Autor: Christoph Rohde
- @retuned eine Zufallszahl aus einen Intervall (Start- & Endwert inklusive).
- */
-int generateRendomNum(){
-    //TODO: Zufallszahl erzeugen und zurückgeben.
-    return 1;
-}
 
-/**
- Prüft ob eine Zufallszahl enthalten ist.
- @true -> erzeugt neue Zufallszahl und prüft erneut.
- @false -> gibt die Zahl zurück.
- */
-int getRendomNum()
-{
-    int rendomNum = 1;
-    
-    while(rowValues.contains(rendomNum)){
-        rendomNum = getRendomNum();
+void draw_grid(sf::RenderWindow &window) {
+    //Draw the matrix of cells
+
+//    const sf::Vector2i resolution = {150, 150}; // TODO: look at the cpp version
+    for (int i = 0; i < 9; i++) {
+
+        for (int j = 0; j < 9; j++) {
+            sf::RectangleShape rectangle(sf::Vector2f(100, 100));
+
+            int size = 150;
+            int &size_ref = size;
+
+            rectangle.setOutlineColor(sf::Color::Blue);
+            rectangle.setOutlineThickness(5);
+            rectangle.setPosition(i * size_ref + 100, j * size_ref +  size_ref);
+            rectangle.setOrigin(size_ref / 2, size_ref / 2);
+
+            window.draw(rectangle);
+        }
     }
-    return rendomNum;
 }
 
-
-template <typename T>
-T getSum(T value1, T value2){
-    return value1 +value2;
-}
 
 int main()
 {
-    
-//    for(int row = 0; row < 9; row++){
-//        int current = getRendomNum();
-//        rowValues.insert(current, current);
-//
-//    }
-    
+//    int rows = 9, columns = 9;
+//    int min = 1, max = 9;
 
-    int i = 23;
-    int j = 321;
-    std::cout <<"\n "<< getSum<int>(i, j);
-    
-    float a = 32.543;
-    float b = 4.22;
-    std::cout <<"\n "<< getSum<float>(a, b);
-    
-    
-    
-    std::string query;             //For multiline with auto formart
-    query = "select * from tabell";
-    query += "  where x > 0";
-    query += "  order by desc";
+//    Matrix sudoku(rows, columns);
+//    sudoku.print_as_console_log();
+
+
+
+
+
+    sf::RenderWindow window(sf::VideoMode(1500, 1800), "Sudoku",
+                            sf::Style::Titlebar | sf::Style::Close);
+
+    window.setFramerateLimit(30);
+
+    // Loop
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear( sf::Color::White);
+
+        draw_grid(window);
+
+
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 }
 
 
